fbeeefa587b18a0eadf14675c7897f8a1233322b
moe-Ali <mohamd1234505@outlook.com>
2023-04-19 03:10:26 +0200

    test

--- src/main/java/com/example/MyServlet.java
+++ src/main/java/com/example/MyServlet.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class MyServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String message = StringUtils.defaultIfBlank(System.getProperty("my-message"), "Hello, From Mohamed Ali! DevOps Porject!!!");
+    String message = StringUtils.defaultIfBlank(System.getProperty("my-message"), "Hello, From Mohamed Ali! DevOps Porject!!");
     response.setContentType("text/html");
     response.getWriter().println("<html><body><h1>" + message + "</h1></body></html>");
   }
